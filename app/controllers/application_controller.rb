5379d65ed8715cb0c0211e299cdaefb54994995a
Carolina <carolina@carolinas-air.mynet>
2019-04-19 09:36:33 +0100

    Create home page route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, '87Cdx4Jl'
+  end
+
+  get '/' do
+    erb :index
+  end
+
 end
