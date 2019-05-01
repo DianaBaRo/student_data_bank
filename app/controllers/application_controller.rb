32d33a3ef10231c9ac75bee203da172862daacb2
Carolina <carolina@carolinas-air.mynet>
2019-05-01 14:13:54 +0100

    Fix typo and change to a require relative link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require './config/environment'
+require_relative '../../config/environment'
 require 'securerandom'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +8,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  end
 
   get '/' do
     erb :index
