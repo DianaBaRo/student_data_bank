da217e4bd2145f58722fb3f0e7f22b6d9a5eb397
Carolina <carolina@carolinas-air.mynet>
2019-05-04 18:16:36 +0100

    Add slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   has_many :students
   has_many :activities
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
