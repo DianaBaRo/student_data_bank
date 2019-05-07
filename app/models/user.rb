01ec3774afb5495b44940940e30949fa4a0072a7
Carolina <carolina@carolinas-air.mynet>
2019-05-07 23:26:46 +0100

    Delete slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,4 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   has_many :students
   has_many :activities
-
-  def slug
-    username.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
-  end
 end
